419d7153e79e4e22e93835cb3b29dab812715db0
Fabian Quoidbach <fabianquoidbach@gmail.com>
2020-09-24 14:24:13 +0200

    Server is connected to DB

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,10 @@
 
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const {APP_PORT} = process.env;
 
 const app = express();
@@ -19,6 +22,16 @@ app.get("/hello", (req, res) => {
     console.log(`ℹ️  (${req.method.toUpperCase()}) ${req.url}`);
     res.send("Hello, World!");
 });
+
+try {
+    mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("Mongo connected");
+} catch (error) {
+    console.log(error);
+}
 
 app.listen(APP_PORT, () =>
     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
