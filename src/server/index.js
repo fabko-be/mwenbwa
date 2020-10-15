05592fbc60c004b915d3b8cefec50919c4a0ee41
Fabian Quoidbach <fabianquoidbach@gmail.com>
2020-10-15 10:43:40 +0200

    prepare to merge

--- src/server/index.js
+++ src/server/index.js
@@ -33,8 +33,8 @@ const app = express();
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
-        "Access-Control-Allow-Headers",+-
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+        "Access-Control-Allow-Headers",
+        +-"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
     );
     res.setHeader(
         "Access-Control-Allow-Methods",
