2dbe1613b5b4d8e4bf5a04c89b9c264c8d95e0c7
Fabian Quoidbach <fabianquoidbach@gmail.com>
2020-10-20 16:54:25 +0200

    Configuration Axios Revue Mise en place du fichier config.js pour récupération baseURL

--- src/client/components/main.js
+++ src/client/components/main.js
@@ -8,17 +8,28 @@
 
 import * as React from "react";
 import {Map, TileLayer} from "react-leaflet";
-// import "../assets/css/index.css";
+// import {importAllTrees} from "../axios/config";
 
-export default function Main() {
-    return (
-        <Map center={[50.6451381, 5.5734203]} zoom={15}>
-            <TileLayer
-                url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
-                attribution={
-                    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                }
-            />
-        </Map>
-    );
+export default class Main extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            lat: 50.6451381,
+            lon: 5.5734203,
+            zoom: 15,
+        };
+    }
+    render() {
+        const position = [this.state.lat, this.state.lon];
+        return (
+            <Map center={position} zoom={this.state.zoom}>
+                <TileLayer
+                    url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+                    attribution={
+                        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    }
+                />
+            </Map>
+        );
+    }
 }
