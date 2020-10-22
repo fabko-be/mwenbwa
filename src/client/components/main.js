d510a0b30c6d3c55fe894fe06e33f10ff2a7e028
Fabian Quoidbach <fabianquoidbach@gmail.com>
2020-10-22 12:36:10 +0200

    Optimisation du chargement de la Map et du Timeout

--- src/client/components/main.js
+++ src/client/components/main.js
@@ -8,21 +8,76 @@
 
 import * as React from "react";
 import {Map, TileLayer} from "react-leaflet";
-// import {importAllTrees} from "../axios/config";
+import api from "../axios/config";
 
 export default class Main extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            lat: 50.6451381,
-            lon: 5.5734203,
-            zoom: 15,
+            trees: [],
+            boundsno: [],
+            boundsse: [],
+        };
+        this.timer = null;
+        this.handleMoveMap = this.handleMoveMap.bind(this);
+        this.currentMap = React.createRef();
+
+        this.handleTimeout = () => {
+            this.timer = setTimeout(() => {
+                this.handleMoveMap();
+            }, 1000);
         };
     }
+
+    componentDidMount() {
+        api.get("/treeslist").then(res => {
+            const trees = res.data;
+            this.setState({trees});
+        });
+        this.handleTimeout();
+    }
+    componentDidUpdate() {
+        clearTimeout(this.timer);
+        this.timer = null;
+    }
+    componentWillUnmount() {
+        clearTimeout(this.timer);
+        this.timer = null;
+    }
+
+    handleMoveMap() {
+        const myMap = this.currentMap.current;
+        const bounds = myMap.leafletElement.getBounds();
+        const nordOuest = bounds.getNorthWest();
+        const sudEst = bounds.getSouthEast();
+        if (
+            this.state.boundsno !== nordOuest ||
+            this.state.boundsse !== sudEst
+        ) {
+            this.setState({
+                boundsno: [nordOuest.lat, nordOuest.lng],
+                boundsse: [sudEst.lat, sudEst.lng],
+            });
+        }
+    }
     render() {
-        const position = [this.state.lat, this.state.lon];
         return (
-            <Map center={position} zoom={this.state.zoom}>
+            <Map
+                center={[50.6451381, 5.5734203]}
+                zoom={17}
+                maxZoom={18}
+                minZoom={17}
+                onMoveEnd={this.handleTimeout}
+                ref={this.currentMap}>
+                {/* {this.state.trees.map(tree => (
+                    <Marker
+                        key={tree._id}
+                        position={[
+                            tree.location.coordinates[1],
+                            tree.location.coordinates[0],
+                        ]}
+                    />
+                ))} */}
                 <TileLayer
                     url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
                     attribution={
